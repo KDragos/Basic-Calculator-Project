083f17ddc26b036da9b60978737286c484b41060
kdragos <kristindragos@gmail.com>
2015-01-18 12:03:36 -0700

    Changed conditions to use a variable.

--- actionpage.php
+++ actionpage.php
@@ -7,23 +7,23 @@ $op = $_GET["op"];
 if (is_numeric($num1) && is_numeric($num2)) {
 	if ($op == "add") {
 		$sum = $num1 + $num2;
-		echo "$num1 + $num2  = $sum";
+		$answer = "$num1 + $num2  = $sum";
 	} else if ($op == "subtract") {
 		$diff = $num1 - $num2;
-		echo "$num1 - $num2 = $diff";
+		$answer = "$num1 - $num2 = $diff";
 	} else if ($op == "multiply") {
 		$product = $num1 * $num2;
-		echo "$num1 * $num2 = $product";
+		$answer = "$num1 * $num2 = $product";
 	} else if ($num2 == 0 && $op == "divide") {
-		echo "You can't divide by zero. Please try another pair of numbers.";
+		$answer = "You can't divide by zero. Please try another pair of numbers.";
 	} else if ($op == "divide") {
 		$dividend = $num1 / $num2;
-		echo "$num1 &#247 $num2 = $dividend";
+		$answer = "$num1 &#247 $num2 = $dividend";
 	} else {
-		echo "Oops. Looks like we missed something.";
+		$answer = "Oops. Looks like we missed something.";
 	}
 } else {
-	echo "Looks like one you entered something that isn't a number. Please try again.";
+	$answer = "Looks like one you entered something that isn't a number. Please try again.";
 }
 
 
@@ -36,7 +36,9 @@ if (is_numeric($num1) && is_numeric($num2)) {
  		<title>Your Answer</title>
  	</head>
  	<body>
- 	
+ 		
+ 		<?php echo $answer ?>
+
  		Want to do more math? <a href="index.php">Go Back</a>
  	
  	</body>
